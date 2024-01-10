b7c21c6831387e22c5165613fc8dc727b7462bad
Git-Senin <lawrencecole8@yahoo.com>
2024-01-10 02:31:08 -0800

    all styling
    +wrapper
    +header/weather icon transitions

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./components/Header/Header.jsx"
 import Footer from "./components/Footer/Footer.jsx"
 import Weather from "./components/Weather/Weather.jsx"
+import "./index.scss"
 
 const App = () => {
   return(
     <>
-      <Header />
-      <Weather location="Sacramento" rain={30} day={50} night={35}/>
-      <Footer />
+      <div className="wrapper">
+        <Header />
+        <Weather location="Sacramento" rain={30} day={50} night={35}/>
+        <Footer />
+      </div>
     </>
   )
 }
--- my-react-app/src/components/Footer/Footer.jsx
+++ my-react-app/src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import styles from "./Footer.module.scss"
 function Footer() {
     return(
         <footer className={styles.Footer}>
+            <hr />
             <p>&copy; {new Date().getFullYear()} Weather</p>
         </footer>
     );
