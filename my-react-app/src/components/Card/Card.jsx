bfd5663723e35797e46e223ecff04364b5ea902b
Git-Senin <lawrencecole8@yahoo.com>
2024-06-06 00:36:25 -0700

    style: card classes

--- my-react-app/src/components/Card/Card.jsx
+++ my-react-app/src/components/Card/Card.jsx
@@ -3,8 +3,8 @@ import styles from "./Card.module.scss"
 export default function Card({link, img, alt}) {
     return (
     <>
-        <a href={link} className={styles.Card}>
-            <img src={img} alt={alt} />
+        <a href={link} className={styles.Card} target="_blank" rel="noopener noreferrer">
+            <img className={styles.Card__img} src={img} alt={alt} />
         </a>
     </>
     )
