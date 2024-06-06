7e6aff0ae93bc9825f9e3d1b1a0f8b068b8df754
Git-Senin <lawrencecole8@yahoo.com>
2024-06-06 00:36:01 -0700

    feat: github/linkin svg

--- my-react-app/src/components/Footer/Footer.jsx
+++ my-react-app/src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import Card from "../Card/Card";
 import styles from "./Footer.module.scss"
+import GithubSVG from "../../assets/Octicons-mark-github.svg"
+import LinkedinSVG from "../../assets/linkedin.svg"
 
 export default function Footer() {
     return(
         <footer className={styles.Footer}>
-            <p>&copy; {new Date().getFullYear()} Weather</p>
-            <Card link="https://github.com/Git-Senin" img="../../assets/Octicons-mark-github.svg" alt="github"/>
+            <Card link="https://github.com/Git-Senin" img={GithubSVG} alt="github"/>
+            <Card link="https://www.linkedin.com/in/8a392424a/" img={LinkedinSVG} alt="linkedin"/>
         </footer >
     );
 }
