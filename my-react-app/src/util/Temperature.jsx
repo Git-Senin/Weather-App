9279da5b1d406b379878442b54e782f457918717
Git-Senin <lawrencecole8@yahoo.com>
2024-06-06 00:34:58 -0700

    Update Temperature.jsx

--- my-react-app/src/util/Temperature.jsx
+++ my-react-app/src/util/Temperature.jsx
@@ -1,11 +1,28 @@
-export function FormatTemperature(n, measurement) {
-    return `${n.toPrecision(4)}°${measurement}`;
-}
+export default class Temperature {
+    constructor(n = 0, measurement = "K"){
+        this.n = n;
+        this.measurement = measurement;
+    }
 
-export function KelvinToCelsius(n) {
-    return (n - 273.15);
-}
+    setTemperature(n) {
+        if(n === null) return null
+        this.n = n;
+        return this;
+    }
 
-export function KelvinToFahrenheit(n) {
-    return ((n - 273.15) * 9/5 + 32);
+    formatTemperature() {
+        return `${this.n.toPrecision(4)}°${this.measurement}`;
+    }
+    
+    kelvinToCelsius() {
+        this.n = (this.n - 273.15);
+        this.measurement = 'C'
+        return this;
+    }
+    
+    kelvinToFahrenheit() {
+        this.n = ((this.n - 273.15) * 9/5 + 32);
+        this.measurement = 'F'
+        return this;
+    }
 }
