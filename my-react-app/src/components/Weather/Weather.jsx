db077d4dab7136afc488c29f6668c8b5a3678ba1
Git-Senin <lawrencecole8@yahoo.com>
2024-06-15 12:41:58 -0700

    fix: tag change
    div descendent of p

--- my-react-app/src/components/Weather/Weather.jsx
+++ my-react-app/src/components/Weather/Weather.jsx
@@ -10,11 +10,11 @@ export default function Weather(prop) {
         <section className={styles.Weather}>
             <img className={styles.Weather__img} src="https://cdn.iconscout.com/icon/free/png-256/free-weather-191-461610.png?f=webp" alt="weather-icon"/>
             <h2 className={styles.Weather__location}>{prop.data.name || "A City"}</h2>
-            <p className={styles.Weather__report}>
+            <div className={styles.Weather__report}>
                 <div>{fahrenheit}</div>
                 <div>{celsius}</div>
                 <div>{kelvin}</div>
-            </p>
+            </div>
         </section>
     )
 }
