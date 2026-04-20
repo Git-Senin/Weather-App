82f0b15249c3bdcc8dec1f73eab5053df741f7bd
Lawrence <lawrencecole8@yahoo.com>
2026-04-19 19:53:43 -0700

    docker implementation

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: true,
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://server:8080',
         changeOrigin: true,
         secure: false,
       },
